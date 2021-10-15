7a699c6ad78de0f14dd75df0a2e33566a5704ca6
Joseph D. Harvey <jdharvey@us.ibm.com>
2021-10-15 12:35:43 -0400

    build: testing

--- packages/logging/src/main/index.ts
+++ packages/logging/src/main/index.ts
@@ -1,6 +1,7 @@
 function doStuff() {
     console.log('did stuff!')
     console.log('it is a change!')
+    console.log('it is yet another change!')
 }
 
 export { doStuff }
--- services/logging-service/src/main/index.ts
+++ services/logging-service/src/main/index.ts
@@ -3,5 +3,6 @@ import {doStuff} from '@carbon-platform/logging'
 doStuff()
 
 console.log("it's an interesting change!")
+console.log("it's another interesting change!")
 
 console.log('hello')

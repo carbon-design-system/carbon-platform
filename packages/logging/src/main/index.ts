2fa95755df7932fa095026c8efadb78c29e8b459
Andrea Cardona <andreancardona@gmail.com>
2021-10-15 15:18:41 -0500

    fix: bumped version number - testing testing lerna

--- packages/logging/src/main/index.ts
+++ packages/logging/src/main/index.ts
@@ -2,7 +2,7 @@ function doStuff() {
     console.log('did stuff!')
     console.log('it is a change!')
     console.log('it is yet another change!')
-    console.log('it is yet another and another change!')
+    console.log('it is yet another & another change!')
 }
 
 export { doStuff }

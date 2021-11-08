24315f96432939382824e079a69d8ac6557c3a6d
Matt Rosno <matt.rosno@ibm.com>
2021-11-08 13:44:15 -0600

    feat: wire up components page

--- services/web-app/components/catalog-list/catalog-list.js
+++ services/web-app/components/catalog-list/catalog-list.js
@@ -13,39 +13,51 @@ import {
   Svg24Cloud,
   Svg24React
 } from '@carbon-platform/icons'
+
 import { Add16 } from '@carbon/icons-react'
+import Link from 'next/link'
+import { getStatus } from '@/utils/schema'
+import slugify from 'slugify'
 import styles from './catalog-list.module.scss'
 
-const CatalogList = () => {
+const CatalogList = ({ assets = [] }) => {
   return (
-    <div className={styles.item}>
-      <div className={styles.itemImage}>
-        <Add16 aria-label="Add" className="my-custom-class" />
-      </div>
-      <div className={styles.itemContent}>
-        <p className={styles.itemSponsor}>{'Super Sponsor'}</p>
-        <header className={styles.itemName}>{'The best component ever'}</header>
-        <p className={styles.itemDescription}>
-          {
-            "This component is so powerful. It's actually the best component in the world if you didn't know"
-          }
-        </p>
-        <footer className={styles.itemInfo}>
-          <Svg12Stable className={styles.itemStatusIcon} />
-          <div className={styles.itemStatus}>{'Stable'}</div>
-          <Svg14Download className={styles.itemDownloadsIcon} />
-          <div className={styles.itemDownloads}>{'1,234'}</div>
-          <Svg14License className={styles.itemLicenseIcon} />
-          <div className={styles.itemLicense}>{'Apache 2.0'}</div>
-          <Svg16Carbon className={styles.itemReviewedIcon} />
-          <div className={styles.itemReviewed}>{'Reviewed'}</div>
-        </footer>
-      </div>
-      <div className={styles.itemTags}>
-        <Svg24React />
-        <Svg24Cloud className={styles.itemTagsSponsor} />
-      </div>
-    </div>
+    <ul>
+      {assets.map((asset, i) => (
+        <li className={styles.item} key={i}>
+          <Link
+            href={`/assets/${asset.params.slug}/latest/${slugify(asset.content.name, {
+              lower: true
+            })}`}
+          >
+            <a className={styles.itemInner}>
+              <div className={styles.itemImage}>
+                <Add16 aria-label="Add" className="my-custom-class" />
+              </div>
+              <div className={styles.itemContent}>
+                <p className={styles.itemSponsor}>{'Super Sponsor'}</p>
+                <header className={styles.itemName}>{asset.content.name}</header>
+                <p className={styles.itemDescription}>{asset.content.description}</p>
+                <footer className={styles.itemInfo}>
+                  <Svg12Stable className={styles.itemStatusIcon} />
+                  <div className={styles.itemStatus}>{getStatus(asset.content.status)}</div>
+                  <Svg14Download className={styles.itemDownloadsIcon} />
+                  <div className={styles.itemDownloads}>{'1,234'}</div>
+                  <Svg14License className={styles.itemLicenseIcon} />
+                  <div className={styles.itemLicense}>{'Apache 2.0'}</div>
+                  <Svg16Carbon className={styles.itemReviewedIcon} />
+                  <div className={styles.itemReviewed}>{'Reviewed'}</div>
+                </footer>
+              </div>
+              <div className={styles.itemTags}>
+                <Svg24React />
+                <Svg24Cloud className={styles.itemTagsSponsor} />
+              </div>
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
--- services/web-app/pages/assets/elements.js
+++ services/web-app/pages/assets/elements.js
@@ -7,12 +7,11 @@
 
 import { useContext, useEffect } from 'react'
 
+import CatalogIndexPage from '@/components/catalog-index-page'
 import { LayoutContext } from '@/layouts/layout'
-import Link from 'next/link'
 import { NextSeo } from 'next-seo'
 import { assetsNavData } from '@/data/nav-data'
 import { getAllLibraries } from '@/lib/github'
-import slugify from 'slugify'
 import styles from '@/pages/pages.module.scss'
 
 const Elements = ({ librariesData }) => {
@@ -26,37 +25,10 @@ const Elements = ({ librariesData }) => {
     setNavData(assetsNavData)
   }, [setNavData])
 
-  const libraries = librariesData.libraries
-    .filter((library) => library.assets.length)
-    .sort((a, b) =>
-      a.content.name > b.content.name ? 1 : b.content.name > a.content.name ? -1 : 0
-    )
-
-  const assets = libraries
-    .reduce((assets, library) => {
-      return assets.concat(library.assets)
-    }, [])
-    .filter((asset) => asset.content.type === 'element')
-    .sort((a, b) =>
-      a.content.name > b.content.name ? 1 : b.content.name > a.content.name ? -1 : 0
-    )
-
   return (
     <>
       <NextSeo {...seo} />
-      <ul>
-        {assets.map((asset, i) => (
-          <li key={i}>
-            <Link
-              href={`/assets/${asset.params.slug}/latest/${slugify(asset.content.name, {
-                lower: true
-              })}`}
-            >
-              <a>{asset.content.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <CatalogIndexPage data={librariesData} type="element" />
       <pre className={styles.data}>{JSON.stringify(librariesData, null, 2)}</pre>
     </>
   )

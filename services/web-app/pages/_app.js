b4adbb0637012621c0691d358f5d85bac6b6f2e0
Joseph D. Harvey <jdharvey@us.ibm.com>
2021-11-10 09:59:35 -0500

    fix(web-app): reorder imports to avoid carbon prefix bug
    - This is a temporary solution until the Carbon bug is fixed.

--- services/web-app/pages/_app.js
+++ services/web-app/pages/_app.js
@@ -4,13 +4,20 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+/* eslint-disable simple-import-sort/imports */
+/*
+ * TODO: fix import sorting once Carbon prefix bug is resolved.
+ * Layout must be imported before carbon-components-react
+ * Details: https://github.com/carbon-design-system/carbon/discussions/9723
+ */
 import '@/styles/styles.scss'
+
+import Layout, { LayoutProvider } from '@/layouts/layout'
 
 import { unstable_FeatureFlags as FeatureFlags } from 'carbon-components-react'
 import { DefaultSeo } from 'next-seo'
 
 import defaultSeo from '@/config/seo.json'
-import Layout, { LayoutProvider } from '@/layouts/layout'
 
 function App({ Component, pageProps }) {
   return (

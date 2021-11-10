9e6c108c16a43e3ba4af2898105a2d2851cd214f
francinelucca <francinelucca@hotmail.com>
2021-11-10 11:51:56 -0500

    refactor: server on logging-service instead of logging package

--- packages/logging/src/main/index.ts
+++ packages/logging/src/main/index.ts
@@ -4,18 +4,13 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-const http = require('http')
+function doStuff() {
+  console.log('did stuff!')
+  console.log('it is a change!')
+  console.log('it is yet another change!')
+  console.log('it is yet another & another change!')
+  console.log('it is yet another & another change haha!')
+  console.log('small change!')
+}
 
-http
-  .createServer(function (request: any, response: any) {
-    console.log('request ', request.url)
-
-    let filePath = '.' + request.url
-    if (filePath === './') {
-      filePath = './index.html'
-    }
-    response.writeHead(200)
-    response.end()
-  })
-  .listen(process.env.PORT)
-console.log(`Server running at port ${process.env.PORT}`)
+export { doStuff }

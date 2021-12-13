74215b6b2bb9a512928d048a22c6719db85ccd53
Matt Rosno <matt.rosno@ibm.com>
2021-12-13 08:00:24 -0600

    feat: upgrade carbon react to 0.11.0

--- services/web-app/components/catalog-pagination/catalog-pagination.js
+++ services/web-app/components/catalog-pagination/catalog-pagination.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { Column, Grid, Layer, Pagination, Theme } from '@carbon/react'
+import { Column, Grid, Pagination } from '@carbon/react'
 
 import { mediaQueries, useMatchMedia } from '@/utils/use-match-media'
 
@@ -19,28 +19,30 @@ const CatalogPagination = ({
 }) => {
   const isMd = useMatchMedia(mediaQueries.md)
 
+  /**
+   * @todo the Pagination component is missing icons for next page and previous page using the
+   * latest `@carbon/react@0.11.0`. This has been reported:
+   * {@link https://github.com/carbon-design-system/carbon/discussions/10247}.
+   */
+
   return (
-    <Theme className={styles.container} theme="white">
-      <Layer>
-        <Grid condensed={!isMd} narrow={isMd}>
-          <Column className={styles.column} sm={4} md={8} lg={12}>
-            <Pagination
-              onChange={({ page, pageSize }) => {
-                if (pageSize !== currentPageSize) {
-                  setPageSize(pageSize)
-                } else {
-                  setPage(page)
-                }
-              }}
-              page={currentPage}
-              pageSize={currentPageSize}
-              pageSizes={[12, 24, 48, 96]}
-              totalItems={assets.length}
-            />
-          </Column>
-        </Grid>
-      </Layer>
-    </Theme>
+    <Grid className={styles.container} condensed={!isMd} narrow={isMd}>
+      <Column className={styles.column} sm={4} md={8} lg={12}>
+        <Pagination
+          onChange={({ page, pageSize }) => {
+            if (pageSize !== currentPageSize) {
+              setPageSize(pageSize)
+            } else {
+              setPage(page)
+            }
+          }}
+          page={currentPage}
+          pageSize={currentPageSize}
+          pageSizes={[12, 24, 48, 96]}
+          totalItems={assets.length}
+        />
+      </Column>
+    </Grid>
   )
 }
 

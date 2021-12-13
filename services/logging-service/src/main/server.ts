fa46af13cb55815106ec6132d514f21943998985
Joseph D. Harvey <jdharvey@us.ibm.com>
2021-12-13 16:47:35 -0500

    fix(logging): merge conflicts

--- services/logging-service/src/main/server.ts
+++ services/logging-service/src/main/server.ts
@@ -4,8 +4,8 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { doStuff } from '@carbon-platform/logging'
-import http, { IncomingMessage, ServerResponse } from 'http'
+import { Logging } from '@carbon-platform/logging'
+import http from 'http'
 
 const port = process.env.PORT || 3000
 const logging = new Logging('logging-service')
@@ -17,9 +17,10 @@ function requestHandler(request: http.IncomingMessage, response: http.ServerResp
   response.end()
 }
 
-  http
-    .createServer(function (request: IncomingMessage, response: ServerResponse) {
-      console.log('request ', request?.url)
+function run() {
+  http.createServer(requestHandler).listen(port)
+  logging.info(`Server running at port ${port}`)
+}
 
 const __test__ = {
   requestHandler

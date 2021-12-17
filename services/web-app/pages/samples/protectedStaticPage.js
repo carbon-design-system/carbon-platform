b95e9839dfa2271ccfa24ca26bc91bd4241a592b
francinelucca <francinelucca@hotmail.com>
2021-12-17 13:08:42 -0500

    feat(auth): add sample protected static page

---
+++ services/web-app/pages/samples/protectedStaticPage.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright IBM Corp. 2021, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { useAuth } from 'contexts/auth'
+import { useEffect } from 'react'
+
+import RequireAuth from '@/components/requireAuth'
+
+import FourOhFour from '../404'
+
+const ProtectedStaticPage = () => {
+  const { isAuthenticated, loading, user } = useAuth()
+
+  useEffect(() => {
+    if (!loading && isAuthenticated) {
+      // fetch protected data here
+    }
+  }, [loading, isAuthenticated])
+
+  // eslint-disable-next-line multiline-ternary
+  return loading ? null : (
+    // show page if user is authenticated and email ends with ibm.com, else show 404 page
+    <RequireAuth
+      fallback={FourOhFour}
+      isAuthorized={isAuthenticated && user?.email?.endsWith('ibm.com')}
+    >
+      <>
+        Welcome to the Protected Page!
+        <div>User: {JSON.stringify(user ?? {})}</div>
+        <div>isAuthenticated: {isAuthenticated?.toString()}</div>
+        <div>isLoading: {loading?.toString()}</div>
+      </>
+    </RequireAuth>
+  )
+}
+
+// do NOT fetch protected data here
+export async function getStaticProps(/* context */) {
+  return {
+    props: {} // will be passed to the page component as props
+  }
+}
+
+export default ProtectedStaticPage

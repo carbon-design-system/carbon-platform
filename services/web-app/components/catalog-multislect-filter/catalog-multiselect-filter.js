d621912963b53574d78eac96f1fe351e6c23d0e9
Matt Rosno <matt.rosno@ibm.com>
2022-01-07 11:25:10 -0600

    chore: redeploy

--- services/web-app/components/catalog-multislect-filter/catalog-multiselect-filter.js
+++ services/web-app/components/catalog-multislect-filter/catalog-multiselect-filter.js
@@ -18,7 +18,7 @@ import styles from './catalog-multiselect-filter.module.scss'
 
 /**
  * @todo (1) close popover on escape key, (2) move focus to popover on open, (3) update state on
- * close so the modal doesn't close each time you select a new item
+ * close so the modal doesn't close each time you select a new item.
  */
 const CatalogMultiselectFilter = ({ filter, className: customClassName, onFilter }) => {
   const [open, setOpen] = useState(false)

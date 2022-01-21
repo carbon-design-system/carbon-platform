8c643b05ea5798bcd2386d5dda921f49cfec7dea
Matt Rosno <matt.rosno@ibm.com>
2022-01-21 14:02:57 -0600

    feat: use run mode instead of env var

--- services/web-app/config/constants.js
+++ services/web-app/config/constants.js
@@ -4,5 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export const CACHE_PATH = process.env.FILE_SYSTEM_CACHE || '.carbon-oss'
+import { getRunMode, PROD } from '@carbon-platform/api/run-mode'
+
+export const CACHE_PATH = getRunMode() === PROD ? '.carbon' : '.carbon-oss'
 export const IMAGES_CACHE_PATH = '.carbon'

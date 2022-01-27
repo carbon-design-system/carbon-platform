a6d5035f39149a0aae0e1df046d83770613bfdfc
Joe Harvey <51208233+jdharvey-ibm@users.noreply.github.com>
2022-01-27 15:56:11 -0500

    Update services/web-app/middleware/requireSession.js

--- services/web-app/middleware/requireSession.js
+++ services/web-app/middleware/requireSession.js
@@ -4,12 +4,6 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-/*
- * Copyright IBM Corp. 2021, 2021
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import { getPassportInstance, SESSION_SECRET, store } from '@carbon-platform/api/auth'
 import { getRunMode, PROD } from '@carbon-platform/api/run-mode'
 import cookieParser from 'cookie-parser'

7cdad7f4696cb0519c5f4b2a0573522b4fa8ff49
Andrea N. Cardona <andreancardona@gmail.com>
2022-02-10 00:40:14 -0600

    feat: need to clean up hover and border

--- services/web-app/pages/assets/[host]/[org]/[repo]/[library]/[ref]/[asset].js
+++ services/web-app/pages/assets/[host]/[org]/[repo]/[library]/[ref]/[asset].js
@@ -7,7 +7,7 @@
 
 import { TextInput } from '@carbon/pictograms-react'
 import { Column, Grid } from '@carbon/react'
-import { ArrowRight } from '@carbon/react/icons'
+import { ArrowRight, Launch } from '@carbon/react/icons'
 import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -63,6 +63,8 @@ const Asset = ({ libraryData }) => {
   }
 
   const [assetData] = libraryData.assets
+  const type = assetData.content.type
+  const assetType = type.charAt(0).toUpperCase() + type.slice(1)
   const { name, description, inherits: inheritsData, thumbnailData: imageData } = assetData.content
   const assetFramework = assetData.content.framework
   const framework = frameworkMap[assetFramework].name
@@ -124,59 +126,58 @@ const Asset = ({ libraryData }) => {
             aspectRatio={{ sm: '2x1', md: '1x1', lg: '3x4', xlg: '1x1' }}
             border={['sm']}
           >
-            <p className={dashboardStyles.title}>Maintainer</p>
+            <p className={dashboardStyles.title}>Library</p>
             <h3 className={dashboardStyles.titleLarge}>{libraryData.content.name}</h3>
-            <AssetItemMeta
-              className={clsx(dashboardStyles.title, styles.meta)}
-              properties={['status']}
-            />
-            {SponsorIcon && <SponsorIcon className={styles.metaAbsolute} size={64} />}
+            <p className={clsx(styles.metaLinkLarge, dashboardStyles.subcolumn)}>
+              {'v.' + libraryData.content.version}
+            </p>
+            {SponsorIcon && <SponsorIcon className={styles.metaAbsolute} size={32} />}
           </DashboardItem>
         </Column>
         <Column className={dashboardStyles.column} lg={2}>
           <DashboardItem aspectRatio={{ sm: '1x1', lg: 'none', xlg: 'none' }} border={['sm']}>
             <Grid columns={2} className={dashboardStyles.subgrid}>
               <Column className={clsx(styles.metaInfo, dashboardStyles.subcolumn)}>
-                <p className={dashboardStyles.title}>Library</p>
+                <p className={dashboardStyles.title}>Sponsor</p>
                 <Link href={`${assetPath}`}>
                   <a className={styles.metaLink}>{libraryData.content.name}</a>
                 </Link>
               </Column>
               <Column>
-                <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>Version</p>
-                <h3 className={clsx(dashboardStyles.subtitle, dashboardStyles.subcolumn)}>
-                  {libraryData.content.version}
-                </h3>
-              </Column>
-              <Column>
-                <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>License</p>
-                <h3 className={dashboardStyles.subtitle}>{libraryData.content.license}</h3>
+                <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>Type</p>
+                <h3 className={dashboardStyles.subtitle}>{assetType}</h3>
               </Column>
               <Column>
                 <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>Framework</p>
                 <h3 className={dashboardStyles.subtitle}>{framework}</h3>
               </Column>
               <Column>
-                <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>
-                  Page last modified
-                </p>
-                <h3 className={dashboardStyles.subtitle}>June 30, 2022</h3>
+                <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>Status</p>
+                <AssetItemMeta
+                  className={clsx(dashboardStyles.title, styles.meta)}
+                  properties={['status']}
+                />
+              </Column>
+              <Column className={dashboardStyles.subcolumnLinks}>
+                <p className={clsx(dashboardStyles.title)}>Demo links</p>
+                <Link href={`${assetPath}`}>
+                  <a className={styles.metaLink}>{libraryData.content.name}</a>
+                </Link>
+                <Link href={`${assetPath}`}>
+                  <a className={styles.metaLink}>{libraryData.content.name}</a>
+                </Link>
               </Column>
               <Column>
-                <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>
-                  Primary design kit
-                </p>
-                <h3 className={dashboardStyles.subtitle}>Sketch</h3>
+                <p className={clsx(dashboardStyles.title, dashboardStyles.subcolumn)}>Type</p>
+                <h3 className={dashboardStyles.subtitle}>{'some content'}</h3>
               </Column>
             </Grid>
-            <Column className={dashboardStyles.designKit}>
-              <Link href={`${assetPath}`} className={dashboardStyles.designKitText}>
-                <a>
-                  <div className={dashboardStyles.designKitTitle}>Get the kits</div>
-                  <ArrowRight className={dashboardStyles.designKitIcon} size={16} />
-                </a>
-              </Link>
-            </Column>
+            <Link href={`${assetPath}`}>
+              <a className={dashboardStyles.linkContainer}>
+                <div className={dashboardStyles.linkText}>Get the kits</div>
+                <ArrowRight size={16} />
+              </a>
+            </Link>
           </DashboardItem>
         </Column>
         <Column className={dashboardStyles.column} sm={0} md={1}>
@@ -186,9 +187,10 @@ const Asset = ({ libraryData }) => {
                 aspectRatio={{ md: '2x1', lg: '16x9', xlg: '2x1' }}
                 border={['sm', 'md', 'lg', 'xlg']}
               >
-                <p className={dashboardStyles.title}>Weekly downloads</p>
-                <h3 className={dashboardStyles.titleLarge}>{'+' + libraryData.response.size}</h3>
+                <p className={dashboardStyles.title}>Pull requests</p>
+                <h3 className={dashboardStyles.titleLarge}>{libraryData.response.size}</h3>
                 {SponsorIcon && <SponsorIcon className={styles.metaAbsolute} size={32} />}
+                <Launch className={styles.metaRightAbsolute} size={20} />
               </DashboardItem>
             </a>
           </Link>
@@ -203,6 +205,7 @@ const Asset = ({ libraryData }) => {
                 <p className={dashboardStyles.title}>Pull requests</p>
                 <h3 className={dashboardStyles.titleLarge}>{libraryData.response.size}</h3>
                 {SponsorIcon && <SponsorIcon className={styles.metaAbsolute} size={32} />}
+                <Launch className={styles.metaRightAbsolute} size={20} />
               </DashboardItem>
             </a>
           </Link>
@@ -217,12 +220,10 @@ const Asset = ({ libraryData }) => {
                 <p className={dashboardStyles.title}>Open issues</p>
                 <h3 className={dashboardStyles.titleLarge}>{libraryData.response.size}</h3>
                 {SponsorIcon && <SponsorIcon className={styles.metaAbsolute} size={32} />}
+                <Launch className={styles.metaRightAbsolute} size={20} />
               </DashboardItem>
             </a>
           </Link>
-          <Column>
-            {SponsorIcon && <SponsorIcon className={styles.metaAbsolute} size={32} />}
-          </Column>
         </Column>
         <Column className={dashboardStyles.column} sm={0} md={1} lg={0}>
           <DashboardItem

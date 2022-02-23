4cfd32cf75b414b073428856fa0247067555ff02
francinelucca <francinelucca@hotmail.com>
2022-02-23 15:08:36 -0500

    chore(web-app): do not show login button if already logged in

--- services/web-app/components/PageNotFound/PageNotFound.js
+++ services/web-app/components/PageNotFound/PageNotFound.js
@@ -6,48 +6,44 @@
  */
 import { Button, Column, Grid } from '@carbon/react'
 import { ArrowRight } from '@carbon/react/icons'
+import { useEffect, useState } from 'react'
 
-// import { useAuth } from '@/contexts/auth'
-// import { isValidIbmEmail } from '@/utils/string'
-// import RequireAuth from '../auth/require-auth'
+import { useAuth } from '@/contexts/auth'
+import { isValidIbmEmail } from '@/utils/string'
+
 import styles from './PageNotFound.module.scss'
 
 export const PageNotFound = () => {
-  //  const { isAuthenticated, loading, user } = useAuth()
+  const [isAuthorized, setisAuthorized] = useState(false)
+  const { isAuthenticated, loading, user } = useAuth()
 
-  // const loginGrid =
+  useEffect(() => {
+    if (!loading && isAuthenticated && isValidIbmEmail(user?.email ?? '')) {
+      setisAuthorized(true)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading, isAuthenticated, user])
 
   return (
-    <>
-      {/* <RequireAuth
-      fallback={FourOhFour}
-      isAuthorized={isAuthenticated && isValidIbmEmail(user?.email ?? '')}
-    >
-      <>
-        Welcome to the Protected Page!
-        <div>User: {JSON.stringify(user ?? {})}</div>
-        <div>isAuthenticated: {isAuthenticated?.toString()}</div>
-        <div>isLoading: {loading?.toString()}</div>
-      </>
-    </RequireAuth> */}
-      <Grid className={styles.grid}>
-        <Column className={styles.column} sm={4} md={6} lg={6} xlg={6} max={6}>
-          <h1 className={styles.title}>Page not found.</h1>
+    <Grid className={styles.grid}>
+      <Column className={styles.column} sm={4} md={6} lg={6} xlg={6} max={6}>
+        <h1 className={styles.title}>Page not found.</h1>
+        {!isAuthorized && (
           <Grid>
             <Column sm={4} md={8} lg={12} xlg={12} max={12}>
               <h1 className={styles.title}>Log in to view all pages.</h1>
+              <Grid columns={3}>
+                <Column sm={3} md={2} lg={2} xlg={2} max={2}>
+                  <Button className={styles.loginButton} as="a" href="/api/login">
+                    Log in
+                    <ArrowRight size={16} />
+                  </Button>
+                </Column>
+              </Grid>
             </Column>
-            <Grid columns={{ md: 12 }}>
-              <Column md={8} lg={8} xlg={8} max={8}>
-                <Button className={styles.loginButton} as="a" href="/api/login">
-                  Log in
-                  <ArrowRight size={16} />
-                </Button>
-              </Column>
-            </Grid>
           </Grid>
-        </Column>
-      </Grid>
-    </>
+        )}
+      </Column>
+    </Grid>
   )
 }

7f7bab4f37867646b7b15bb7c7c4bb9f003b6124
Matt Rosno <matt.rosno@ibm.com>
2022-02-25 15:26:36 -0600

    chore: shorten variables
    - remove variables used only once
    - use semantic elements

--- services/web-app/pages/assets/index.js
+++ services/web-app/pages/assets/index.js
@@ -24,7 +24,7 @@ import HeroImg from './index/images/hero-illo.png'
 import styles from './index/index.module.scss'
 
 const PageContent = () => {
-  const pageHighlights = [
+  const highlights = [
     {
       header: 'Better discoverability',
       title: 'For designers and developers:',
@@ -41,23 +41,23 @@ const PageContent = () => {
     }
   ]
 
-  const pageVersionReleases = [
+  const releases = [
     {
       version: 'v0.1',
-      targetRelease: 'Target release:',
+      release: 'Target release:',
       date: 'February 27',
       title: 'Standardize assets and libraries',
       description:
         // eslint-disable-next-line max-len
         'This private beta is the start to standardizing Carbon so we can bring everything together as one system. This release is focused on helping PAL maintainers classify libraries and assets through the application of a common schema. This release also prototypes asset discovery improvements as Carbon gets indexed through the application of the schema.',
       image: ReleaseOneImg,
-      imageTitle: 'v0.1 User experience:',
-      imageDescription:
+      captionHeading: 'v0.1 User experience',
+      caption:
         'Catalog assets link directly out to Carbon Design System and PAL sites to access an asset’s usage documentation.'
     },
     {
       version: 'v0.2',
-      targetRelease: 'Target release:',
+      release: 'Target release:',
       date: 'March 27',
       title: 'Asset catalog experience',
       description:
@@ -71,14 +71,14 @@ const PageContent = () => {
         'Replace legacy community component index with beta catalogs'
       ],
       image: ReleaseTwoImg,
-      imageTitle: 'v0.2 User experience:',
-      imageDescription:
+      captionHeading: 'v0.2 User experience',
+      caption:
         // eslint-disable-next-line max-len
         'Users can begin to access some library and asset docs without leaving the platform; however the user will still need to go offsite for more detailed usage documentation.'
     },
     {
       version: 'v1.0',
-      targetRelease: 'Target release:',
+      release: 'Target release:',
       date: 'May 9',
       title: 'General availability',
       description:
@@ -89,14 +89,14 @@ const PageContent = () => {
         'Global asset catalog no longer uses external links to legacy docs sites'
       ],
       image: ReleaseThreeImg,
-      imageTitle: 'v1.0 User experience:',
-      imageDescription:
+      captionHeading: 'v1.0 User experience',
+      caption:
         // eslint-disable-next-line max-len
         'This release replaces Carbon’s current website — all 32 “core” components will have complete asset detail pages. If a PAL has migrated content, then it will begin to remove its external docs urls to leverage the detail pages. User log in will be fully integrated into the experience as well.'
     },
     {
       version: 'v1.1',
-      targetRelease: 'Target release:',
+      release: 'Target release:',
       date: 'June',
       title: 'Explore with confidence',
       subtitle: 'This release will fully take the following Hill:',
@@ -108,14 +108,14 @@ const PageContent = () => {
         'New code package and component usage analytics'
       ],
       image: ReleaseFourImg,
-      imageTitle: 'v1.1 User experience:',
-      imageDescription:
+      captionHeading: 'v1.1 User experience',
+      caption:
         // eslint-disable-next-line max-len
         'In this release Standards content will be available on the platform and component usage analytics will allow us to track component insertions.'
     },
     {
       version: 'v2.0',
-      targetRelease: 'Target release:',
+      release: 'Target release:',
       date: 'Q3',
       title: 'System of systems',
       subtitle: 'The platform’s second major release will take the following Hills:',
@@ -142,8 +142,8 @@ const PageContent = () => {
         'Visual content authoring experience with less reliance on markdown'
       ],
       image: ReleaseFiveImg,
-      imageTitle: 'v2.0 User experience:',
-      imageDescription:
+      captionHeading: 'v2.0 User experience',
+      caption:
         // eslint-disable-next-line max-len
         'In this release, all external docs content has been fully migrated allowing the PALs to sunset. The team switcher will allow teams to view assets and documenation through the lens of their team.'
     }
@@ -153,40 +153,40 @@ const PageContent = () => {
     <div className={styles.content}>
       <Grid>
         <Column sm={4} md={8} lg={8} xlg={7}>
-          <p className={styles.pageDescription}>
+          <h2 className={styles.pageDescription}>
             The new Carbon Design System will provide a single place to find and use all open and
             inner source assets teams need to build consistent, scalable experiences with
             confidence.
-          </p>
+          </h2>
         </Column>
       </Grid>
       <div className={styles.background}>
         <Grid className={styles.releaseContainer}>
           <Column sm={4} md={4} lg={4} xlg={4}>
-            <div className={clsx(styles.header, styles.headerRelease)}>This release</div>
+            <h3 className={clsx(styles.header, styles.headerRelease)}>This release</h3>
           </Column>
           <Column sm={4} md={4} lg={8} xlg={7}>
-            <div className={clsx(styles.description, styles.descriptionRelease)}>
+            <p className={clsx(styles.description, styles.descriptionRelease)}>
               Our first release is the start to standardizing our community’s assets and surfacing
               them in one shared catalog.
-            </div>
+            </p>
           </Column>
         </Grid>
-        {pageHighlights.map((pageHighlight, i) => (
+        {highlights.map((highlight, i) => (
           <Grid className={styles.highlightsContainer} key={i}>
             <Column md={4} lg={4} xlg={4} key={i}>
-              <div className={styles.header}>{pageHighlight.header}</div>
+              <h3 className={styles.header}>{highlight.header}</h3>
             </Column>
             <Column md={4} lg={8} xlg={7}>
-              <div className={styles.title}>{pageHighlight.title}</div>
-              <div className={styles.description}>{pageHighlight.description}</div>
+              <p className={styles.title}>{highlight.title}</p>
+              <p className={styles.description}>{highlight.description}</p>
             </Column>
           </Grid>
         ))}
       </div>
       <Grid className={styles.contentContainer}>
         <Column sm={4} md={8} lg={8} xlg={8}>
-          <h1 className={clsx(styles.header, styles.headerContent)}>How PAL teams can prepare</h1>
+          <h2 className={clsx(styles.header, styles.headerContent)}>How PAL teams can prepare</h2>
           <p className={clsx(styles.text, styles.text)}>
             Ensure your components, functions, patterns and templates are indexed in our unified
             asset discovery experience by March 30th.
@@ -201,7 +201,7 @@ const PageContent = () => {
           >
             <a className={styles.link}>{'Get started'}</a>
           </Link>
-          <h1 className={clsx(styles.header, styles.headerContent)}>{'Platform roadmap'}</h1>
+          <h2 className={clsx(styles.header, styles.headerContent)}>Platform roadmap</h2>
           <p className={clsx(styles.text, styles.text)}>
             Progress on the following releases are documented in{' '}
             <Link href="https://github.com/carbon-design-system/carbon-platform">
@@ -216,61 +216,53 @@ const PageContent = () => {
           </p>
         </Column>
       </Grid>
-      {pageVersionReleases.map((versionRelease, i) => (
+      {releases.map((release, i) => (
         <Grid
           className={clsx(styles.highlightsContainer, styles.highlightsContainerVersion)}
           key={i}
         >
           <Column md={8} lg={4} xlg={4} key={i}>
-            <div className={clsx(styles.header, styles.headerVersion)}>
-              {versionRelease.version}
-            </div>
-            <div className={clsx(styles.header, styles.headerTargetRelease)}>
-              {versionRelease.targetRelease}
-            </div>
-            <div className={clsx(styles.header, styles.headerDate)}>{versionRelease.date}</div>
+            <h2 className={clsx(styles.header, styles.headerVersion)}>{release.version}</h2>
+            <h2 className={clsx(styles.header, styles.headerTargetRelease)}>{release.release}</h2>
+            <h2 className={clsx(styles.header, styles.headerDate)}>{release.date}</h2>
           </Column>
           <Column md={8} lg={8} xlg={7}>
-            <div className={clsx(styles.title, styles.titleVersion)}>{versionRelease.title}</div>
-            <div className={styles.subtitle}>
-              {versionRelease.subtitle ? versionRelease.subtitle : null}
-            </div>
-            <div
+            <h3 className={clsx(styles.title, styles.titleVersion)}>{release.title}</h3>
+            <p className={styles.subtitle}>{release.subtitle ? release.subtitle : null}</p>
+            <p
               className={clsx(
-                versionRelease.subtitle
-                  ? styles.versionDescription
-                  : styles.versionDescriptionPaddingNone
+                release.subtitle ? styles.versionDescription : styles.versionDescriptionPaddingNone
               )}
             >
-              {versionRelease.description}
-            </div>
-            {versionRelease.descriptionBlock &&
-              versionRelease.descriptionBlock.map((descriptionBlock, o) => (
+              {release.description}
+            </p>
+            {release.descriptionBlock &&
+              release.descriptionBlock.map((descriptionBlock, o) => (
                 <div className={styles.descriptionBlockText} key={o}>
-                  <div>{descriptionBlock.title}</div>
-                  <div>{descriptionBlock.description}</div>
+                  <p>{descriptionBlock.title}</p>
+                  <p>{descriptionBlock.description}</p>
                 </div>
               ))}
-            {versionRelease.bulletPointsHeader && (
-              <div className={styles.bulletpointsHeader}>{versionRelease.bulletPointsHeader}</div>
+            {release.bulletPointsHeader && (
+              <p className={styles.bulletpointsHeader}>{release.bulletPointsHeader}</p>
             )}
             <ul className={styles.bulletpoints}>
-              {versionRelease.bulletPoints &&
-                versionRelease.bulletPoints.map((bulletPoint, e) => {
+              {release.bulletPoints &&
+                release.bulletPoints.map((bulletPoint, e) => {
                   return <li key={e}>{'– ' + bulletPoint}</li>
                 })}
             </ul>
             <div className={styles.versionImage}>
               <Image
-                alt={versionRelease.imageTitle}
+                alt={release.captionHeading}
                 layout="responsive"
-                src={versionRelease.image}
+                src={release.image}
                 placeholder="blur"
               />
-              <div className={styles.imageTextContainer}>
-                <h1 className={styles.imageTitle}>{versionRelease.imageTitle}</h1>
-                <p className={styles.imageDescription}>{versionRelease.imageDescription}</p>
-              </div>
+              <p className={styles.imageTextContainer}>
+                <span className={styles.captionHeading}>{release.captionHeading}:</span>
+                <span className={styles.caption}>{release.caption}</span>
+              </p>
             </div>
           </Column>
         </Grid>

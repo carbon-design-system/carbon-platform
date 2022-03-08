3062b216df5057fa49789a3a6dcd2ec2babc5c03
Alison Joseph <alison.joseph@us.ibm.com>
2022-03-08 12:12:40 -0600

    chore: fix build

--- services/web-app/pages/_app.js
+++ services/web-app/pages/_app.js
@@ -6,11 +6,29 @@
  */
 import '@/styles/styles.scss'
 
+import { MDXProvider } from '@mdx-js/react'
 import { DefaultSeo } from 'next-seo'
 
+import Link from '@/components/link'
+import { Blockquote, H1, H2, H3, H4, H5, H6, LI, OL, P, UL } from '@/components/markdown'
 import defaultSeo from '@/config/seo.json'
 import { AuthProvider } from '@/contexts/auth'
 import Layout, { LayoutProvider } from '@/layouts/layout'
+
+const components = {
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6,
+  p: P,
+  ul: UL,
+  ol: OL,
+  li: LI,
+  a: Link,
+  blockquote: Blockquote
+}
 
 function App({ Component, pageProps }) {
   return (
@@ -18,7 +36,9 @@ function App({ Component, pageProps }) {
       <LayoutProvider>
         <Layout>
           <DefaultSeo {...defaultSeo} />
-          <Component {...pageProps} />
+          <MDXProvider components={components}>
+            <Component {...pageProps} />
+          </MDXProvider>
         </Layout>
       </LayoutProvider>
     </AuthProvider>

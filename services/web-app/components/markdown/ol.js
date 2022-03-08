4f92e3d6325b1d8293c71f89b9536554a2c224db
Alison Joseph <alison.joseph@us.ibm.com>
2022-03-08 12:55:25 -0600

    feat: add mdx

---
+++ services/web-app/components/markdown/ol.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright IBM Corp. 2022, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { Column, Grid, OrderedList } from '@carbon/react'
+import cx from 'classnames'
+
+import styles from './markdown.module.scss'
+
+const Ol = ({ children, className, ...rest }) => {
+  return (
+    <Grid>
+      <Column sm={4} md={6} lg={8}>
+        <OrderedList className={cx(className, styles.list, styles.ol)} {...rest}>
+          {children}
+        </OrderedList>
+      </Column>
+    </Grid>
+  )
+}
+
+export default Ol

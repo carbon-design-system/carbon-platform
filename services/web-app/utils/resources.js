c70534a56b66795d3b68c72c889cc8c0164579d2
Francine Lucca <40550942+francinelucca@users.noreply.github.com>
2022-03-16 11:42:19 -0400

    Update services/web-app/utils/resources.js
    Co-authored-by: Joe Harvey <51208233+jdharvey-ibm@users.noreply.github.com>

--- services/web-app/utils/resources.js
+++ services/web-app/utils/resources.js
@@ -4,12 +4,6 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-// /*
-//  * Copyright IBM Corp. 2022, 2022
-//  *
-//  * This source code is licensed under the Apache-2.0 license found in the
-//  * LICENSE file in the root directory of this source tree.
-//  */
 
 import assetValidationSchema from '@/validation/asset-validation.schema.json'
 import libraryValidationSchema from '@/validation/library-validation.schema.json'

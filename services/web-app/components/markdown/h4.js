0ecf51fb9c03c361aa5dbf52729555a866ecf0af
Alison Joseph <alison.joseph@us.ibm.com>
2022-03-22 12:32:43 -0500

    fix: clsx imports

--- services/web-app/components/markdown/h4.js
+++ services/web-app/components/markdown/h4.js
@@ -5,13 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 import { Column, Grid } from '@carbon/react'
-import cx from 'classnames'
+import clsx from 'clsx'
 
 import styles from './markdown.module.scss'
 
 const H4 = ({ children, className, ...rest }) => {
   return (
-    <Grid className={cx(className, styles.h4Container)} {...rest}>
+    <Grid className={clsx(className, styles.h4Container)} {...rest}>
       <Column sm={4} md={6} lg={8}>
         <h4 className={styles.h4}>{children}</h4>
       </Column>

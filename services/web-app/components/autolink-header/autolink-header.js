95da1982b0bfa2717ac2e37ee2660965caf4feea
Alison Joseph <alisonejoseph@Gmail.com>
2022-03-23 13:13:07 -0500

    Update autolink-header.js

--- services/web-app/components/autolink-header/autolink-header.js
+++ services/web-app/components/autolink-header/autolink-header.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { Link } from '@carbon/react/icons'
+import { Link } from '@carbon/react/icons' 
 import clsx from 'clsx'
 // eslint-disable-next-line no-use-before-define
 import React from 'react'

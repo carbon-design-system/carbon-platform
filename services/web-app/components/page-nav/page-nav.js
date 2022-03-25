56e2901f4c0a9cc73fb38d3c49c1b35b03e9dd01
Alison Joseph <alison.joseph@us.ibm.com>
2022-03-25 12:47:48 -0500

    wip: wip

--- services/web-app/components/page-nav/page-nav.js
+++ services/web-app/components/page-nav/page-nav.js
@@ -6,6 +6,8 @@
  */
 import { Column, Grid } from '@carbon/react'
 import clsx from 'clsx'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
 import { useCallback, useEffect } from 'react'
 
@@ -15,40 +17,43 @@ import { mediaQueries, useMatchMedia } from '@/utils/use-match-media'
 import styles from './page-nav.module.scss'
 
 const PageNav = ({ contentRef, items = [] }) => {
+  const router = useRouter()
   const isLg = useMatchMedia(mediaQueries.lg)
 
-  const navLinks = contentRef.current.querySelectorAll('[class^="page-nav_link"]')
-  const sections = contentRef.current.querySelectorAll('[id]')
+  // const navLinks = contentRef.current.querySelectorAll('[class^="page-nav_link"]')
+  // const sections = contentRef.current.querySelectorAll('[id]')
 
   const scrollHandler = useCallback(() => {
-    sections.forEach((section) => {
-      const sectionHeight = section.offsetHeight
-      const sectionTopDistance = section.getBoundingClientRect().top
-      const scrollDistance = 90
-
-      if (
-        sectionTopDistance < scrollDistance &&
-        sectionHeight + sectionTopDistance - scrollDistance > 0
-      ) {
-        navLinks.forEach((link) => {
-          if (section.id === link.dataset.id) {
-            link.classList.add(styles.linkActive)
-          }
-        })
-      } else {
-        navLinks.forEach((link) => {
-          if (section.id === link.dataset.id) {
-            link.classList.remove(styles.linkActive)
-          }
-        })
-      }
-    })
+    console.log('scrolling')
+    // sections.forEach((section) => {
+    //   const sectionHeight = section.offsetHeight
+    //   const sectionTopDistance = section.getBoundingClientRect().top
+    //   const scrollDistance = 90
+    //   if (
+    //     sectionTopDistance < scrollDistance &&
+    //     sectionHeight + sectionTopDistance - scrollDistance > 0
+    //   ) {
+    //     navLinks.forEach((link) => {
+    //       if (section.id === link.dataset.id) {
+    //         link.classList.add(styles.linkActive)
+    //       }
+    //     })
+    //   } else {
+    //     navLinks.forEach((link) => {
+    //       if (section.id === link.dataset.id) {
+    //         link.classList.remove(styles.linkActive)
+    //       }
+    //     })
+    //   }
+    // })
   })
 
   useEventListener('scroll', scrollHandler)
+
   useEffect(() => {
     scrollHandler()
-  }, [scrollHandler])
+    console.log(contentRef)
+  }, [contentRef, scrollHandler])
 
   return (
     <Grid narrow={isLg} className={styles.container}>
@@ -58,9 +63,16 @@ const PageNav = ({ contentRef, items = [] }) => {
             {items.map((item, i) => (
               <li className={styles.item} key={i}>
                 {item.id && (
-                  <a href={`#${item.id}`} data-id={item.id} className={clsx(styles.link)}>
-                    {item.title}
-                  </a>
+                  <Link href={`#${item.id}`}>
+                    <a
+                      className={clsx(
+                        styles.link,
+                        router.asPath.includes(item.id) ? styles.linkActive : ''
+                      )}
+                    >
+                      {item.title}
+                    </a>
+                  </Link>
                 )}
               </li>
             ))}

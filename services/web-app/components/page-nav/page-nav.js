12a28462ac38e3f0f701015fe786ffc7f2321377
Alison Joseph <alison.joseph@us.ibm.com>
2022-03-29 07:17:03 -0500

    wip: trying to get sticky nav to work

--- services/web-app/components/page-nav/page-nav.js
+++ services/web-app/components/page-nav/page-nav.js
@@ -9,9 +9,10 @@ import clsx from 'clsx'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
-import { useCallback, useEffect } from 'react'
+// import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
 
-import useEventListener from '@/utils/use-event-listener'
+// import useEventListener from '@/utils/use-event-listener'
 import { mediaQueries, useMatchMedia } from '@/utils/use-match-media'
 
 import styles from './page-nav.module.scss'
@@ -20,40 +21,92 @@ const PageNav = ({ contentRef, items = [] }) => {
   const router = useRouter()
   const isLg = useMatchMedia(mediaQueries.lg)
 
-  // const navLinks = contentRef.current.querySelectorAll('[class^="page-nav_link"]')
-  // const sections = contentRef.current.querySelectorAll('[id]')
+  useEffect(() => {
+    const handleRAF = () => {
+      window.requestAnimationFrame(setSelectedItem)
+    }
 
-  const scrollHandler = useCallback(() => {
-    console.log('scrolling')
-    // sections.forEach((section) => {
-    //   const sectionHeight = section.offsetHeight
-    //   const sectionTopDistance = section.getBoundingClientRect().top
-    //   const scrollDistance = 90
-    //   if (
-    //     sectionTopDistance < scrollDistance &&
-    //     sectionHeight + sectionTopDistance - scrollDistance > 0
-    //   ) {
-    //     navLinks.forEach((link) => {
-    //       if (section.id === link.dataset.id) {
-    //         link.classList.add(styles.linkActive)
-    //       }
-    //     })
-    //   } else {
-    //     navLinks.forEach((link) => {
-    //       if (section.id === link.dataset.id) {
-    //         link.classList.remove(styles.linkActive)
-    //       }
-    //     })
-    //   }
-    // })
+    window.addEventListener('scroll', handleRAF)
+    return () => window.removeEventListener('scroll', handleRAF)
   })
 
-  useEventListener('scroll', scrollHandler)
+  const setSelectedItem = () => {
+    const navLinks = contentRef.current.querySelectorAll('[class^="page-nav_link"]')
+    const sections = contentRef.current.querySelectorAll('[id]')
 
-  useEffect(() => {
-    scrollHandler()
-    console.log(contentRef)
-  }, [contentRef, scrollHandler])
+    sections.forEach((section) => {
+      const sectionHeight = section.offsetHeight
+      const sectionTopDistance = section.getBoundingClientRect().top
+      // Space between top of screen and where we want section to be "Active"
+      const scrollDistance = 90
+      if (
+        // Setting active class when the top of the section is at the top
+        // of the scree nand the bottom of the section is visible
+        sectionTopDistance < scrollDistance &&
+        sectionHeight + sectionTopDistance - scrollDistance > 0
+      ) {
+        navLinks.forEach((link) => {
+          if (section.id === link.dataset.id) {
+            link.classList.add(styles.linkActive)
+
+            // Set url to current link as you scroll past
+            // This works to set the url, but breaks
+            // TODO - create issue?
+            // This is broken
+            // router.push(link.href, undefined, { shallow: true })
+          }
+        })
+      } else {
+        navLinks.forEach((link) => {
+          if (section.id === link.dataset.id) {
+            link.classList.remove(styles.linkActive)
+          }
+        })
+      }
+    })
+  }
+
+  // const scrollHandler = useCallback(() => {
+  //   // if (!contentRef && !contentRef.current) return
+  //   const navLinks = contentRef.current.querySelectorAll('[class^="page-nav_link"]')
+  //   const sections = contentRef.current.querySelectorAll('[id]')
+  //   sections.forEach((section) => {
+  //     const sectionHeight = section.offsetHeight
+  //     const sectionTopDistance = section.getBoundingClientRect().top
+  //     // Space between top of screen and where we want section to be "Active"
+  //     const scrollDistance = 90
+  //     if (
+  //       // Setting active class when the top of the section is at the top
+  //       // of the scree nand the bottom of the section is visible
+  //       sectionTopDistance < scrollDistance &&
+  //       sectionHeight + sectionTopDistance - scrollDistance > 0
+  //     ) {
+  //       navLinks.forEach((link) => {
+  //         if (section.id === link.dataset.id) {
+  //           link.classList.add(styles.linkActive)
+  //           // Set url to current link as you scroll past
+
+  //           // router.push(link.href, undefined, { shallow: true })
+  //         }
+  //       })
+  //     } else {
+  //       navLinks.forEach((link) => {
+  //         if (section.id === link.dataset.id) {
+  //           link.classList.remove(styles.linkActive)
+  //         }
+  //       })
+  //     }
+  //   })
+  // }, [contentRef])
+
+  // useEventListener('scroll', scrollHandler)
+
+  // useEffect(() => {
+  //   scrollHandler()
+  //   console.log('contentRef: ', contentRef)
+  // }, [contentRef, scrollHandler])
+
+  //  }, [contentRef, scrollHandler])
 
   return (
     <Grid narrow={isLg} className={styles.container}>
@@ -65,6 +118,7 @@ const PageNav = ({ contentRef, items = [] }) => {
                 {item.id && (
                   <Link href={`#${item.id}`}>
                     <a
+                      data-id={item.id}
                       className={clsx(
                         styles.link,
                         router.asPath.includes(item.id) ? styles.linkActive : ''

2b3ec203357c431c11006055065a1a47878ebcfe
Alison Joseph <alison.joseph@us.ibm.com>
2022-04-13 09:23:13 -0500

    feat: add anchor link mdx component

---
+++ services/web-app/components/inline-notification/inline-notification.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright IBM Corp. 2021, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+import { InlineNotification as CarbonInlineNotification } from '@carbon/react'
+import { ErrorFilled, CheckmarkFilled, WarningFilled, InformationFilled } from '@carbon/react/icons'
+import { Column, Grid } from '@carbon/react'
+
+import styles from './inline-notification.module.scss'
+
+const iconTypes = {
+  error: ErrorFilled,
+  success: CheckmarkFilled,
+  warning: WarningFilled,
+  info: InformationFilled
+}
+
+const InlineNotification = ({ children, className, kind = 'info' }) => {
+  const containerClassName = cx(className, {
+    [`cds--inline-notification`]: true,
+    [`cds--inline-notification--low-contrast`]: true,
+    [`cds--inline-notification--${kind}`]: kind,
+    [`cds--inline-notification--hide-close-button`]: true
+  })
+  const IconForKind = iconTypes[kind]
+  if (!IconForKind) {
+    return null
+  }
+
+  return (
+    <Grid>
+      <Column sm={4} md={6} lg={8} className={cx(styles.notification, className)}>
+        <div className={containerClassName}>
+          <div className="cds--inline-notification__details">
+            {IconForKind ? (
+              <IconForKind className="cds--inline-notification__icon" size={20}>
+                <title>{`${kind} icon`}</title>
+              </IconForKind>
+            ) : null}
+            <div className="cds--inline-notification__text-wrapper">
+              <div className={styles.content}>{children}</div>
+            </div>
+          </div>
+        </div>
+      </Column>
+    </Grid>
+  )
+}
+
+InlineNotification.propTypes = {
+  kind: PropTypes.oneOf(['error', 'info', 'success', 'warning'])
+}
+
+export default InlineNotification

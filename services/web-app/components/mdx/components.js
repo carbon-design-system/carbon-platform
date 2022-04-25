cda1b8c11464fed0bb5c186aabeb8c7f2318769f
Alison Joseph <alison.joseph@us.ibm.com>
2022-04-25 09:21:11 -0500

    feat(web-app): add mdx page table

--- services/web-app/components/mdx/components.js
+++ services/web-app/components/mdx/components.js
@@ -20,6 +20,7 @@ import Link from '@/components/link'
 import { Blockquote, H1, H2, H3, H4, H5, H6, Image, LI, OL, P, UL } from '@/components/markdown'
 import MdxWrapper from '@/components/mdx-wrapper'
 import PageDescription from '@/components/page-description'
+import PageTable from '@/components/page-table'
 import Profile from '@/components/profile'
 import ResourceCard from '@/components/resource-card'
 import { Tab, Tabs } from '@/components/tabs'
@@ -59,6 +60,7 @@ const components = {
   a: Link,
   blockquote: Blockquote,
   img: Image,
+  table: PageTable,
   /* Gatsby theme components */
   Caption: Caption,
   PageDescription: PageDescription,

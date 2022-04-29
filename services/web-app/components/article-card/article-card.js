ba64483cd5896dac17ae3e184721cbedb3408e62
Joseph D. Harvey <jdharvey@us.ibm.com>
2022-04-28 23:32:25 -0400

    fix(web-app): import, type, and functional component usage
    - Convert class components to functional components
    - Add 'true' arg to fix carbon type-style issues
    - Remove react default imports
    - Remove usages of @carbon/icons-react

--- services/web-app/components/article-card/article-card.js
+++ services/web-app/components/article-card/article-card.js
@@ -10,94 +10,87 @@ import { ArrowRight, Download, Email, Error, Launch } from '@carbon/react/icons'
 import clsx from 'clsx'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
-import React from 'react'
 
 import styles from './article-card.module.scss'
 
-export default class ArticleCard extends React.Component {
-  render() {
-    const {
-      children,
-      href,
-      title,
-      subTitle,
-      author,
-      date,
-      readTime,
-      color,
-      disabled,
-      actionIcon,
-      className,
-      ...rest
-    } = this.props
+const ArticleCard = (props) => {
+  const {
+    children,
+    href,
+    title,
+    subTitle,
+    author,
+    date,
+    readTime,
+    color,
+    disabled,
+    actionIcon,
+    className,
+    ...rest
+  } = props
 
-    let isLink
-    if (href !== undefined) {
-      isLink = href.charAt(0) === '/'
-    }
+  let isLink
+  if (href !== undefined) {
+    isLink = href.charAt(0) === '/'
+  }
 
-    const ArticleCardClassNames = clsx(className, styles['article-card'], {
-      [styles.disabled]: disabled
-    })
+  const ArticleCardClassNames = clsx(className, styles['article-card'], {
+    [styles.disabled]: disabled
+  })
 
-    const aspectRatioClassNames = clsx('cds--aspect-ratio', 'cds--aspect-ratio--2x1')
+  const aspectRatioClassNames = clsx('cds--aspect-ratio', 'cds--aspect-ratio--2x1')
 
-    const cardContentClassNames = clsx('cds--tile', 'cds--tile--clickable')
+  const cardContentClassNames = clsx('cds--tile', 'cds--tile--clickable')
 
-    const cardContent = (
-      <>
-        <div className={styles.img}>{children}</div>
-        <div className={aspectRatioClassNames}>
-          <div className={clsx('cds--aspect-ratio--object', styles.tile)}>
-            {subTitle && <h5 className={styles.subtitle}>{subTitle}</h5>}
-            {title && <h4 className={styles.title}>{title}</h4>}
-            <div className={styles.info}>
-              {author && <p>{author}</p>}
-              {date && <p>{date}</p>}
-              {readTime && <p>{readTime}</p>}
-            </div>
-            <div className={styles['icon-action']}>
-              {actionIcon === 'launch' && !disabled && <Launch size={20} aria-label="Open" />}
-              {actionIcon === 'arrowRight' && !disabled && (
-                <ArrowRight size={20} aria-label="Open" />
-              )}
-              {actionIcon === 'download' && !disabled && (
-                <Download size={20} aria-label="Download" />
-              )}
-              {actionIcon === 'email' && !disabled && <Email size={20} aria-label="Email" />}
-              {actionIcon === 'disabled' ||
-                (disabled === true && <Error size={20} aria-label="disabled" />)}
-            </div>
+  const cardContent = (
+    <>
+      <div className={styles.img}>{children}</div>
+      <div className={aspectRatioClassNames}>
+        <div className={clsx('cds--aspect-ratio--object', styles.tile)}>
+          {subTitle && <h5 className={styles.subtitle}>{subTitle}</h5>}
+          {title && <h4 className={styles.title}>{title}</h4>}
+          <div className={styles.info}>
+            {author && <p>{author}</p>}
+            {date && <p>{date}</p>}
+            {readTime && <p>{readTime}</p>}
+          </div>
+          <div className={styles['icon-action']}>
+            {actionIcon === 'launch' && !disabled && <Launch size={20} aria-label="Open" />}
+            {actionIcon === 'arrowRight' && !disabled && <ArrowRight size={20} aria-label="Open" />}
+            {actionIcon === 'download' && !disabled && <Download size={20} aria-label="Download" />}
+            {actionIcon === 'email' && !disabled && <Email size={20} aria-label="Email" />}
+            {actionIcon === 'disabled' ||
+              (disabled === true && <Error size={20} aria-label="disabled" />)}
           </div>
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 
-    let cardContainer
-    if (disabled === true) {
-      cardContainer = <div className={cardContentClassNames}>{cardContent}</div>
-    } else if (isLink === true) {
-      cardContainer = (
-        <Link href={href}>
-          <a href={href} className={cardContentClassNames} {...rest}>
-            {cardContent}
-          </a>
-        </Link>
-      )
-    } else {
-      cardContainer = (
+  let cardContainer
+  if (disabled === true) {
+    cardContainer = <div className={cardContentClassNames}>{cardContent}</div>
+  } else if (isLink === true) {
+    cardContainer = (
+      <Link href={href}>
         <a href={href} className={cardContentClassNames} {...rest}>
           {cardContent}
         </a>
-      )
-    }
-
-    return (
-      <Theme theme={color === 'dark' && 'g100'}>
-        <div className={ArticleCardClassNames}>{cardContainer}</div>
-      </Theme>
+      </Link>
+    )
+  } else {
+    cardContainer = (
+      <a href={href} className={cardContentClassNames} {...rest}>
+        {cardContent}
+      </a>
     )
   }
+
+  return (
+    <Theme theme={color === 'dark' && 'g100'}>
+      <div className={ArticleCardClassNames}>{cardContainer}</div>
+    </Theme>
+  )
 }
 
 ArticleCard.defaultProps = {
@@ -157,3 +150,5 @@ ArticleCard.propTypes = {
    */
   title: PropTypes.string
 }
+
+export default ArticleCard

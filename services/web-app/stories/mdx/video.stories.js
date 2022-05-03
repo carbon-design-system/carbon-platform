2675ce587edae78e789569eb1550735b6a1a4c96
Alison Joseph <alison.joseph@us.ibm.com>
2022-05-03 12:19:48 -0500

    feat: add video to storybook

---
+++ services/web-app/stories/mdx/video.stories.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright IBM Corp. 2022, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import Video from '@/components/video'
+import localPoster from '@/pages/mdx/video/local-poster.jpeg'
+import localVideo from '@/pages/mdx/video/local-video.mp4'
+
+const stories = {
+  title: 'MDX/Video',
+  component: Video
+}
+
+export default stories
+
+const Template = (args) => <Video src={localVideo} poster={localPoster} {...args} />
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const Vimeo = Template.bind({})
+Vimeo.args = {
+  title: 'Vimeo example',
+  vimeoId: '310583077',
+  src: '',
+  poster: ''
+}

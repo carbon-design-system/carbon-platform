e49db90e44259efb143d23975c1637958a1822a3
Alison Joseph <alison.joseph@us.ibm.com>
2022-05-04 15:06:56 -0500

    fix: profile image

--- services/web-app/stories/mdx/profile.stories.js
+++ services/web-app/stories/mdx/profile.stories.js
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 import { Grid } from '@carbon/react'
+import Image from 'next/image'
 
 import Profile from '@/components/profile'
 
@@ -18,7 +19,7 @@ export default stories
 const Template = (args) => (
   <Grid condensed>
     <Profile name="Matt Rosno" title="Product Manager" {...args}>
-      <img
+      <Image
         alt="Use markdown in mdx files. ![Matt Rosno headshot](/mdx/rosno_matt.png)"
         src="mdx/rosno_matt.png"
       />

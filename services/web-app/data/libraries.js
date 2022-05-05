275e31d61a67687a5acae2e05a6ad7e25ca1fddb
Andrea N. Cardona <andreancardona@gmail.com>
2022-05-05 14:15:43 -0500

    fix: fix merge

--- services/web-app/data/libraries.js
+++ services/web-app/data/libraries.js
@@ -102,6 +102,7 @@ const prodLibraries = {
     org: 'carbon-design-system',
     repo: 'carbon-components-vue',
     path: '/packages/core/src',
+    group: carbonComponentsGroup
   },
   'carbon-components-svelte': {
     host: 'github.com',

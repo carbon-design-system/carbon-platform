6ecba9ba7c871e923e5d5a5dfa52c4ed100e403e
Alison Joseph <alison.joseph@us.ibm.com>
2022-05-18 09:13:07 -0500

    wip: wip

--- services/web-app/pages/samples/remote-mdx-test.js
+++ services/web-app/pages/samples/remote-mdx-test.js
@@ -24,6 +24,13 @@ const RemoteMdxTest = ({ source }) => {
 
 export const getStaticProps = async () => {
   const mdxSource = await getRemoteMdxData(
+    // {
+    //   host: 'github.com',
+    //   org: 'alisonjoseph',
+    //   repo: 'carbon-website',
+    //   library: 'carbon-website',
+    //   ref: 'platform'
+    // },
     {
       host: 'github.com',
       org: 'carbon-design-system',
@@ -39,10 +46,10 @@ export const getStaticProps = async () => {
 
     // '/src/pages/community/patterns/chatbot/overview.mdx' // has nested <Tag> components
 
-    // '/src/pages/components/dropdown/code.mdx'
-    // '/src/pages/components/code-snippet/code.mdx'
+    '/src/pages/components/dropdown/code.mdx'
+    // /src/pages/data-visualization/complex-charts/index.mdx'
 
-    '/src/pages/components/date-picker/accessibility.mdx'
+    // '/src/pages/components/date-picker/accessibility.mdx'
   )
 
   return {

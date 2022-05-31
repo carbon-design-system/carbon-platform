e8ce787d0d2291a1ff7effcc4ba11f0b3349faec
Francine Lucca <40550942+francinelucca@users.noreply.github.com>
2022-05-31 10:25:25 -0400

    Update services/web-app/components/mini-card/mini-card.js
    Co-authored-by: Alison Joseph <alisonejoseph@Gmail.com>

--- services/web-app/components/mini-card/mini-card.js
+++ services/web-app/components/mini-card/mini-card.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { Column, Link as CarbonLink } from '@carbon/react'
+import { Column } from '@carbon/react'
 import { ArrowRight, Calendar, Download, Email, Launch } from '@carbon/react/icons'
 import clsx from 'clsx'
 import Link from 'next/link'

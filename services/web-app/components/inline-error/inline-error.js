e42d351907308470237a4c8f6ec5ad24b91d67e9
Andrea N. Cardona <andreancardona@gmail.com>
2022-08-02 15:56:31 -0500

    feat: using default inline notification

--- services/web-app/components/inline-error/inline-error.js
+++ services/web-app/components/inline-error/inline-error.js
@@ -4,15 +4,14 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { Column, Grid, Link } from '@carbon/react'
-import { ErrorFilled } from '@carbon/react/icons'
+import { Column, Grid, InlineNotification, Link } from '@carbon/react'
 import clsx from 'classnames'
 import PropTypes from 'prop-types'
 
 import styles from './inline-error.module.scss'
 
 export const InlineError = ({ className, content, title, description, href, link }) => {
-  const containerClassName = clsx(className, {
+  const wrapperClassName = clsx(className, {
     'cds--inline-notification': true,
     'cds--inline-notification--low-contrast': true,
     'cds--inline-notification--error': true
@@ -20,34 +19,18 @@ export const InlineError = ({ className, content, title, description, href, link
 
   return (
     <Grid>
-      <Column sm={4} md={6} lg={12} className={clsx(styles.notification, className)}>
-        <div className={containerClassName}>
-          <Grid>
-            <Column sm={3} md={4} lg={8}>
-              <div className="cds--inline-notification__details">
-                <ErrorFilled className="cds--inline-notification__icon" size={20}>
-                  <title>{'Error icon'}</title>
-                </ErrorFilled>
-                <div className="cds--inline-notification__text-wrapper">
-                  <span className={styles.title}>{title}</span>
-                  <p className={styles.description}>{description}</p>
-                  <p className={styles.content}>{content}</p>
-                </div>
-              </div>
-            </Column>
-            <Column sm={1} md={2} lg={4}>
-              <div className="cds--inline-notification__text-wrapper">
-                <div
-                  className={clsx(styles['link-content'], 'cds--inline-notification__details')}
-                >
-                  <Link className={styles.link} href={href}>
-                    {link}
-                  </Link>
-                </div>
-              </div>
-            </Column>
-          </Grid>
-        </div>
+      <Column sm={4} md={6} lg={12}>
+        <InlineNotification
+          href={href}
+          className={clsx(styles.notification, className, wrapperClassName)}
+        >
+          <span className={styles.container}>
+            <strong className={styles.title}>{title}</strong>
+            <Link className={styles.link}>{link}</Link>
+          </span>
+          <p className={styles.description}>{description}</p>
+          <p className={styles.content}>{content}</p>
+        </InlineNotification>
       </Column>
     </Grid>
   )

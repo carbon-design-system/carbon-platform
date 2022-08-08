d604650a2121bc53583a03a4e6ac2dfa7edfe551
francinelucca <francinelucca@hotmail.com>
2022-08-08 11:25:50 -0400

    feat(web-app): add anchorlinks to mdx-errors page

--- packages/mdx-sanitizer/src/test/mdx-sanitizer-plugin.test.ts
+++ packages/mdx-sanitizer/src/test/mdx-sanitizer-plugin.test.ts
@@ -4,6 +4,11 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import test from 'ava'
+
+test.serial('returns test when the envvar is not set', (t) => {
+  t.is(true, true)
+})
 
 // is there a way to import the mdx without using fs?
 

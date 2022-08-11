e590fa3606421eed994dec42aa01acba3c966c91
Alison Joseph <alisonejoseph@Gmail.com>
2022-08-11 13:03:33 -0500

    Update asset-catalog-item.js

--- services/web-app/components/asset-catalog-item/asset-catalog-item.js
+++ services/web-app/components/asset-catalog-item/asset-catalog-item.js
@@ -89,7 +89,6 @@ const AssetCatalogItemContent = ({ asset, isGrid = false, otherFrameworkCount = 
         )}
         <div className={styles.tags}>
           <TypeTag className={styles['tags-item']} type={asset.content.type} />
-          {console.log(asset.content.type, 'bo')}
           <FrameworkIcon
             className={styles.framework}
             framework={asset.content.framework}

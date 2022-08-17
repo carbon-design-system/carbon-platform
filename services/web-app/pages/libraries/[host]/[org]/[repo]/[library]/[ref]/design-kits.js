93d7512295bb262d7b98d2e8a34fa214c1f5198b
Matt Rosno <matt.rosno@ibm.com>
2022-08-16 19:46:23 -0500

    feat: add page header colors and pictograms
    - add to collection and library pages
    - refactor types to assetTypes data object
    - add new pageHeaders data object
    - add placeholder design kits catalog page
    - remove unused elements catalog page
    - refactor TypeTag to take a name as a prop as gray tag
    - remove Versions page from library, for v1 release

---
+++ services/web-app/pages/libraries/[host]/[org]/[repo]/[library]/[ref]/design-kits.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright IBM Corp. 2022, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { Column, Grid } from '@carbon/react'
+import { useRouter } from 'next/router'
+import { NextSeo } from 'next-seo'
+import { useContext, useEffect } from 'react'
+
+import PageHeader from '@/components/page-header'
+import { assetsNavData } from '@/data/nav-data'
+import { pageHeaders } from '@/data/page-headers'
+import { LayoutContext } from '@/layouts/layout'
+import { getLibraryData, getLibraryNavData } from '@/lib/github'
+
+import pageStyles from '../../../../../../pages.module.scss'
+
+const DesignKits = ({ libraryData, navData }) => {
+  const { setPrimaryNavData, setSecondaryNavData } = useContext(LayoutContext)
+  const router = useRouter()
+
+  const pageHeader = pageHeaders?.designKit ?? {}
+
+  const { name } = libraryData.content
+
+  const seo = {
+    title: `${name} design kits`
+  }
+
+  useEffect(() => {
+    setPrimaryNavData(assetsNavData)
+    setSecondaryNavData(navData)
+  }, [setPrimaryNavData, navData, setSecondaryNavData])
+
+  if (router.isFallback) {
+    return (
+      <Grid>
+        <Column sm={4} md={8} lg={16}>
+          <div className={pageStyles.content}>
+            <h1>Loading...</h1>
+          </div>
+        </Column>
+      </Grid>
+    )
+  }
+
+  return (
+    <>
+      <NextSeo {...seo} />
+      <PageHeader bgColor={pageHeader?.bgColor} title="Design kits" pictogram={pageHeader?.icon} />
+      <Grid>
+        <Column sm={4} md={8} lg={12}>
+          <div className={pageStyles.content}>Coming soon!</div>
+        </Column>
+      </Grid>
+    </>
+  )
+}
+
+export const getServerSideProps = async ({ params }) => {
+  const libraryData = await getLibraryData(params)
+
+  if (!libraryData) {
+    return {
+      notFound: true
+    }
+  }
+
+  const navData = getLibraryNavData(params, libraryData)
+
+  return {
+    props: {
+      libraryData,
+      navData,
+      params
+    }
+  }
+}
+
+export default DesignKits
--- services/web-app/pages/libraries/index.js
+++ services/web-app/pages/libraries/index.js
@@ -13,6 +13,7 @@ import LibraryCatalog from '@/components/library-catalog/library-catalog'
 import PageDescription from '@/components/page-description/page-description'
 import PageHeader from '@/components/page-header'
 import { assetsNavData } from '@/data/nav-data'
+import { pageHeaders } from '@/data/page-headers'
 import { LayoutContext } from '@/layouts/layout'
 import { getAllLibraries } from '@/lib/github'
 import { librarySortComparator } from '@/utils/schema'
@@ -21,6 +22,8 @@ import styles from './index.module.scss'
 
 const Libraries = ({ librariesData }) => {
   const { setPrimaryNavData } = useContext(LayoutContext)
+
+  const pageHeader = pageHeaders?.library ?? {}
 
   const seo = {
     title: 'Libraries'
@@ -37,7 +40,7 @@ const Libraries = ({ librariesData }) => {
   return (
     <>
       <NextSeo {...seo} />
-      <PageHeader title={seo.title} />
+      <PageHeader bgColor={pageHeader?.bgColor} title={seo.title} pictogram={pageHeader?.icon} />
       <PageDescription className={styles['library-description']}>
         Libraries are 1:1 with code packages. All coded components, elements, patterns, or functions
         belong to a library and have a maintainer. Design kits with compatible code also live in

1314ecf6c6f5c983c00f132ccdc56e3301f8e2ec
Alison Joseph <alison.joseph@us.ibm.com>
2022-08-29 13:33:51 -0500

    feat(web-app): lock down iframe props
    closes #1172

--- services/web-app/components/preview/preview.js
+++ services/web-app/components/preview/preview.js
@@ -13,11 +13,28 @@ import styles from './preview.module.scss'
  * The `<Preview>` component is a simple wrapper for an `<iframe />` with
  * styling added to allow it to display responsively within the Platform.
  */
-const Preview = ({ title, ...props }) => (
-  <iframe loading="lazy" title={title} {...props} className={styles.preview} />
+const Preview = ({ title, height, src, style }) => (
+  <iframe
+    src={src}
+    loading="lazy"
+    title={title}
+    height={height}
+    allowtransparency="true"
+    allowFullScreen="true"
+    frameBorder="no"
+    className={styles.preview}
+    style={style}
+    sandbox="allow-forms allow-scripts allow-same-origin"
+  />
 )
 
 Preview.propTypes = {
+  /** Provide the height for the iframe */
+  height: Proptypes.string,
+  /** Provide the url for the iframe */
+  src: Proptypes.string,
+  /** Provide custom inline styles for the iframe */
+  style: Proptypes.object,
   /** Provide the title for the iframe */
   title: Proptypes.string
 }

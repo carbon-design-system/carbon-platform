dea585e6fad89484573e3369ba1a430d6cb0052e
Matt Rosno <matt.rosno@ibm.com>
2022-09-27 16:07:21 -0500

    docs: withLoading jsdoc optional params

--- services/web-app/components/with-loading/with-loading.js
+++ services/web-app/components/with-loading/with-loading.js
@@ -15,20 +15,20 @@ import PageLoading from '@/components/page-loading'
  * Higher-Order Component (HOC) that renders a loading fallback on Incremental Static Regeneration
  * (ISR) pages. You can pass in a config object to pass props to internal React components.
  * @param {React.FC} Component - The original component
- * @param {object} config - The configuration of internal component props
- * @param {object} config.column - Column component prop, see Carbon for full docs
- * @param {(string|object)} config.column.xs
- * @param {(string|object)} config.column.sm
- * @param {(string|object)} config.column.md
- * @param {(string|object)} config.column.lg
- * @param {(string|object)} config.column.xlg
- * @param {(string|object)} config.column.max
- * @param {object} config.pageHeader - PageHeader component prop
- * @param {string} config.pageHeader.bgColor
- * @param {boolean} config.pageHeader.loading
- * @param {object} config.pageHeader.pictogram
- * @param {string} config.pageHeader.title
- * @param {boolean} config.pageHeader.withTabs
+ * @param {object} [config] - The configuration of internal component props
+ * @param {object} [config.column] - Column component prop, see Carbon for full docs
+ * @param {(string|object)} [config.column.xs]
+ * @param {(string|object)} [config.column.sm]
+ * @param {(string|object)} [config.column.md]
+ * @param {(string|object)} [config.column.lg]
+ * @param {(string|object)} [config.column.xlg]
+ * @param {(string|object)} [config.column.max]
+ * @param {object} [config.pageHeader] - PageHeader component prop
+ * @param {string} [config.pageHeader.bgColor]
+ * @param {boolean} [config.pageHeader.loading]
+ * @param {object} [config.pageHeader.pictogram]
+ * @param {string} [config.pageHeader.title]
+ * @param {boolean} [config.pageHeader.withTabs]
  * @returns {React.ReactElement}
  */
 const withLoading = (Component, config = {}) => {

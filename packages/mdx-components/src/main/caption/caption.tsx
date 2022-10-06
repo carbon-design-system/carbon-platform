50c4ec2aa0f4c4f6e00e78ede09e8d56525dfc71
Joseph D. Harvey <jdharvey@us.ibm.com>
2022-10-06 14:40:09 -0400

    fix(mdx-components): update caption class name, add some docs

--- packages/mdx-components/src/main/caption/caption.tsx
+++ packages/mdx-components/src/main/caption/caption.tsx
@@ -7,25 +7,32 @@
 
 import { Column, Grid } from '@carbon/react'
 import { clsx } from 'clsx'
+import PropTypes from 'prop-types'
 import React, { ReactNode } from 'react'
 
+import { MdxComponent } from '../interfaces.js'
 import { withPrefix } from '../utils.js'
 
 interface CaptionProps {
   children: ReactNode
-  className: string
+  className?: string | null
 }
 
 /**
  * The `<Caption>` component is typically used below images or videos.
  */
-const Caption = ({ children, className }: CaptionProps) => (
-  <Grid className={clsx(className, withPrefix('container'))}>
+const Caption: MdxComponent<CaptionProps> = ({ children, className }) => (
+  <Grid className={clsx(className, withPrefix('caption-container'))}>
     <Column sm={4} md={6} lg={6}>
       <p className={withPrefix('caption')}>{children}</p>
     </Column>
   </Grid>
 )
+
+Caption.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string
+}
 
 export { CaptionProps }
 export default Caption

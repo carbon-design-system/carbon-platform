e7ed2a80c3d1007d8caed968d4c7a29c5a08ed03
Alison Joseph <alison.joseph@us.ibm.com>
2022-10-11 14:16:05 -0500

    feat(mdx-components): add use match media

---
+++ packages/mdx-components/src/main/use-match-media.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright IBM Corp. 2021, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { breakpoints } from '@carbon/layout'
+import { useEffect, useState } from 'react'
+
+export const mediaQueries = {
+  sm: `(min-width: ${breakpoints.sm.width})`,
+  md: `(min-width: ${breakpoints.md.width})`,
+  lg: `(min-width: ${breakpoints.lg.width})`,
+  xlg: `(min-width: ${breakpoints.xlg.width})`,
+  max: `(min-width: ${breakpoints.max.width})`
+}
+
+const getMatchMedia = (query: string) => {
+  return typeof window !== 'undefined' ? window.matchMedia(query) : null
+}
+
+export const useMatchMedia = (query: string) => {
+  const [matches, setMatches] = useState(getMatchMedia(query)?.matches)
+
+  useEffect(() => {
+    const matchMediaResult = getMatchMedia(query)
+    const updateMatch = (e: {
+      matches: boolean | ((prevState: boolean | undefined) => boolean | undefined) | undefined
+    }) => {
+      setMatches(e.matches)
+    }
+
+    matchMediaResult?.addEventListener('change', updateMatch)
+
+    return () => {
+      matchMediaResult?.removeEventListener('change', updateMatch)
+    }
+  }, [query])
+
+  return matches
+}

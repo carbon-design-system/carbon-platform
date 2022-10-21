43a97d21f706126212c09de50278ba7e04eb0599
Francine Lucca <francinelucca@hotmail.com>
2022-10-21 13:30:56 -0400

    smartypants regex test

--- services/web-app/retext-smartypants/index.js
+++ services/web-app/retext-smartypants/index.js
@@ -4,6 +4,9 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+/*
+ * From: https://github.com/retextjs/retext-smartypants/blob/main/index.js
+ */
 /**
  * @typedef {import('nlcst').Root} Root
  * @typedef {import('nlcst').Sentence} Sentence

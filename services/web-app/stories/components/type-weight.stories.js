4361c5b6abb9aeb670b864dca8403a997e3df7ba
Francine Lucca <francinelucca@hotmail.com>
2022-11-08 16:41:25 -0500

    fix(web-app): reintroduce deleted stories

---
+++ services/web-app/stories/components/type-weight.stories.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright IBM Corp. 2022, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { TypeWeight } from '@/components/type'
+
+const stories = {
+  title: 'Platform/Components/TypeWeight',
+  component: TypeWeight,
+  argTypes: {},
+  parameters: {}
+}
+
+export default stories
+
+const Template = (args) => <TypeWeight {...args} />
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const Italic = Template.bind({})
+Italic.args = {
+  type: 'italic'
+}
+
+export const Types = Template.bind({})
+Types.args = {
+  type: 'types'
+}

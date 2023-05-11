cb2bb1bc074f0c79a9fc72faf0fc31ecf7d60a9b
Joseph D. Harvey <jdharvey@us.ibm.com>
2023-05-11 15:35:05 -0400

    fix(logging): inject runtime into microservice instantiation

--- services/logging/src/main/index.ts
+++ services/logging/src/main/index.ts
@@ -7,17 +7,21 @@
 import { Logging } from '@carbon-platform/api/logging'
 import { EventMessage, Queue } from '@carbon-platform/api/messaging'
 import { PlatformMicroservice } from '@carbon-platform/api/microservice'
+import { Runtime } from '@carbon-platform/api/runtime'
 
 import { LoggingModule } from './logging-module.js'
 
 async function start() {
+  const runtime = new Runtime()
+
   // Disable remote logging entirely for the logging service
   Logging.isRemoteLoggingAllowed = false
 
   const pm = new PlatformMicroservice({
     queue: Queue.Logging,
     module: LoggingModule,
-    autoAck: true // Remove messages immediately from the queue without explicit ack
+    autoAck: true, // Remove messages immediately from the queue without explicit ack
+    runtime
   })
 
   await pm.bind<EventMessage>('log_logged')
